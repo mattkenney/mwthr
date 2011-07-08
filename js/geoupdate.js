dcf431a444a15d4362137b8bc2bbdd1ebfa42d1c
Matt Kenney <mkenney@mjk-tech.com>
2011-07-07 22:07:37 -0400

    hide address bar; refresh page after 10 minutes

--- js/geoupdate.js
+++ js/geoupdate.js
@@ -1,20 +1,51 @@
 (function(){
 
+if (window.addEventListener)
+{
+    addEventListener(
+        'load',
+        function ()
+        {
+            setTimeout(
+                function ()
+                {
+                    scrollTo(0, 1);
+                },
+                1
+            );
+        },
+        false
+    );
+}
+
 if (navigator.geolocation)
 {
-    navigator.geolocation.getCurrentPosition(
-        function (pos)
+    var start = new Date().getTime();
+
+    setInterval(
+        function ()
         {
-            document.cookie = 'lat=' + pos.coords.latitude;
-            document.cookie = 'lon=' + pos.coords.longitude;
-            document.cookie = 'utc=' + new Date().getTime();
+            var now = new Date().getTime();
+            if (now - start > 600000)
+            {
+                navigator.geolocation.getCurrentPosition(
+                    function (pos)
+                    {
+                        document.cookie = 'lat=' + pos.coords.latitude;
+                        document.cookie = 'lon=' + pos.coords.longitude;
+                        document.cookie = 'utc=' + new Date().getTime();
+                        location = location;
+                    },
+                    null,
+                    {
+                        enableHighAccuracy:false,
+                        maximumAge:1800000,
+                        timeout:6000
+                    }
+                );
+            }
         },
-        null,
-        {
-            enableHighAccuracy:false,
-            maximumAge:1800000,
-            timeout:6000
-        }
+        10000
     );
 }
 
